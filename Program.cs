65caa5b1a5c2393a3a70054bd6e9a3d253d602bd
YakhoubSoumare <yakhoub.soumare1990@gmail.com>
2025-05-12 11:58:21 +0200

    replaced inmemory connection with configured azure db

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TaskManagerContext>(opt => opt.UseInMemoryDatabase("TaskManager")); // InMemory database
+// builder.Services.AddDbContext<TaskManagerContext>(opt => opt.UseInMemoryDatabase("TaskManager")); // InMemory database
 
-// builder.Services.AddDbContext<TaskManagerContext>(opt => 
-//     opt.UseSqlServer(
-//         builder.Configuration.GetConnectionString("AzureConnection"),
-//         providerOptions => providerOptions.EnableRetryOnFailure()
-//     )
-// );
+builder.Services.AddDbContext<TaskManagerContext>(opt => 
+    opt.UseSqlServer(
+        builder.Configuration.GetConnectionString("AzureConnection"),
+        providerOptions => providerOptions.EnableRetryOnFailure()
+    )
+);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,8 +24,8 @@ var app = builder.Build();
 using(var scope = app.Services.CreateAsyncScope()) 
 {
    var dbContext = scope.ServiceProvider.GetRequiredService<TaskManagerContext>();
-   dbContext.Database.EnsureCreated();// in memory scenario
-//    dbContext.Database.Migrate(); // This will apply migrations if any are pending.
+//    dbContext.Database.EnsureCreated();// in memory scenario
+   dbContext.Database.Migrate(); // This will apply migrations if any are pending.
 }
 
 // Configure the HTTP request pipeline.

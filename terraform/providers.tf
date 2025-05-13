0b1aeffc0a2756a71c031da2d8ce484759217bd4
YakhoubSoumare <yakhoub.soumare1990@gmail.com>
2025-05-13 14:57:38 +0200

    Fix Azure provider configuration by adding required provider block and features block

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,6 +11,11 @@ terraform {
     required_version = ">= 1.11.2"
 }
 
+# Configure the Azure provider
+provider "azurerm" {
+    features {} # Enables access to Azure resources
+}
+
 # required_providers ensures we are using the azurerm provider for managing Azure resources.
 
 # features {} is required but can later be customized (e.g., to prevent accidental deletions).
